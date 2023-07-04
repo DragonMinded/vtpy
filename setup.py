ebfe11b275c2255edae7d4009ffd3357d496893b
Jennifer Taylor <dragonminded@dragonminded.com>
2023-07-04 02:44:57 +0000

    Hopefully install py.typed.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="VTPy",
-    version="1.0.2",
+    version="1.0.3",
     description="Python abstraction layer for VT-100 terminals.",
     author="Jennifer Taylor",
     author_email="jen@superjentendo.com",
