71373429c9cca681cd1f5aba9d328f8b39747bc7
Jennifer Taylor <dragonminded@dragonminded.com>
2023-07-04 02:08:44 +0000

    Make into installable.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="VTPy",
+    version="1.0",
+    description="Python abstraction layer for VT-100 terminals.",
+    author="Jennifer Taylor",
+    author_email="jen@superjentendo.com",
+    url="https://github.com/DragonMinded/vtpy",
+    packages=["vtpy"],
+    requires=["pySerial"],
+)
